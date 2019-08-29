f2492dcc5b7292793eb318953b94f37fb35cd78b
Matt Vaughn <vaughn@tacc.utexas.edu>
2019-08-29 05:32:33 -0500

    Example simple management action
    Implemented apps-enable and apps-disable using the 'apps.manage' method. Reponse currently mirrors the service response, which is
    to provide a listing view of the app after the management action has completed.

--- tapis_cli/commands/taccapis/v2/apps/__init__.py
+++ tapis_cli/commands/taccapis/v2/apps/__init__.py
@@ -3,6 +3,9 @@
 
 from .. import SERVICE_VERSION
 from .models import App, API_NAME
+
 from .show import AppsShow
 from .list import AppsList
 from .search import AppsSearch
+from .enable import AppsEnable
+from .disable import AppsDisable
---
+++ tapis_cli/commands/taccapis/v2/apps/disable.py
@@ -0,0 +1,27 @@
+from tapis_cli.display import Verbosity
+from tapis_cli.search import SearchWebParam
+
+from . import API_NAME, SERVICE_VERSION
+from .models import App
+from .formatters import AppsFormatOne
+
+__all__ = ['AppsDisable']
+
+
+class AppsDisable(AppsFormatOne):
+    """Disable an app for usage
+    """
+    VERBOSITY = Verbosity.LISTING
+    EXTRA_VERBOSITY = Verbosity.RECORD
+
+    def take_action(self, parsed_args):
+        super().take_action(parsed_args)
+        headers = App().get_headers(self.VERBOSITY, parsed_args.formatter)
+        rec = self.tapis_client.apps.manage(
+            appId=parsed_args.identifier,
+            body={'action': 'disable'})
+        data = []
+        for key in headers:
+            val = self.render_value(rec.get(key, None))
+            data.append(val)
+        return (tuple(headers), tuple(data))
