7f2f1614d92c5533d85079345a61324dba4e96cf
Matt Vaughn <vaughn@tacc.utexas.edu>
2019-08-31 06:32:49 -0500

    Added systems-create and systems-update

---
+++ tapis_cli/commands/taccapis/v2/systems/create.py
@@ -0,0 +1,29 @@
+from tapis_cli.display import Verbosity
+from tapis_cli.search import SearchWebParam
+from tapis_cli.clients.services.taccapis.v2.bearer import UploadJsonFile, TapisServiceIdentifier
+
+from . import API_NAME, SERVICE_VERSION
+from .models import System
+from .formatters import SystemsFormatOne
+
+__all__ = ['SystemsCreate']
+
+# TODO - enforce use of create vs update by checking for existence of systemId
+
+
+class SystemsCreate(UploadJsonFile, SystemsFormatOne):
+    """Create a new system
+    """
+    VERBOSITY = Verbosity.RECORD
+
+    def take_action(self, parsed_args):
+        super().take_action(parsed_args)
+        headers = System().get_headers(self.VERBOSITY, parsed_args.formatter)
+        self.handle_file_upload(parsed_args)
+
+        rec = self.tapis_client.systems.add(body=self.json_file_contents)
+        data = []
+        for key in headers:
+            val = self.render_value(rec.get(key, None))
+            data.append(val)
+        return (tuple(headers), tuple(data))
