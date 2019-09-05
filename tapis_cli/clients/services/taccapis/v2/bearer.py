50db85dd0246d893edf3b413065d95401636b862
Matt Vaughn <vaughn@tacc.utexas.edu>
2019-09-05 05:04:06 -0500

    Now including the actual Tapis classes

--- tapis_cli/clients/services/taccapis/v2/bearer.py
+++ tapis_cli/clients/services/taccapis/v2/bearer.py
@@ -1,4 +1,5 @@
 import argparse
+import datetime
 import json
 from agavepy.agave import Agave
 
@@ -6,13 +7,17 @@ from cliff.command import Command
 from cliff.hooks import CommandHook
 from cliff.app import App
 from tapis_cli.display import Verbosity
+from tapis_cli.utils import datetime_to_isodate, datetime_to_human
 
 from ...mixins import (AppVerboseLevel, JsonVerbose, UploadJsonFile,
                        ServiceIdentifier)
 from ....oauth import BearerTokenFormatOne, BearerTokenFormatMany
 from .request import Swaggerless
 
-__all__ = ['TaccApisBase', 'TaccApisFormatOne', 'TaccApisFormatMany']
+__all__ = [
+    'TaccApisBase', 'TaccApisFormatOne', 'TaccApisFormatMany',
+    'TaccApisFormatManyUnlimited'
+]
 
 
 def add_common_arguments(parser):
@@ -24,7 +29,28 @@ def add_common_arguments(parser):
 
 
 class TaccApisBase(object):
-    pass
+    # The contents of a search payload can be loaded in here for use
+    # by the Swaggerless API client
+    post_payload = dict()
+
+    def take_action_defaults(self, parsed_args):
+        return self
+
+    def render_value(self, value):
+        """Renders a value basd on current display format
+
+        Each field value in a response can be passed through this function
+        to implement format-specific rendering
+        """
+        # If the value is a date/time and display is a table, render as
+        # a human-readable value instead of an ISO-8601 date
+        if isinstance(value, datetime.datetime):
+            if self.formatter_default == 'table':
+                # TODO - figure out why this only works for the ShowOne
+                value = datetime_to_human(value)
+            else:
+                value = datetime_to_isodate(value)
+        return value
 
 
 class TasApiClient(object):
@@ -72,14 +98,11 @@ class TaccApisFormatMany(JsonVerbose, TasApiClient, BearerTokenFormatMany):
                             help='Skip first O records')
         return parser
 
-    # @property
-    # def formatter_default(self):
-    #     """Overrides formatter_default to return JSON when -v is passed
-    #     """
-    #     if self.app_verbose_level > 1:
-    #         return 'json'
-    #     else:
-    #         return 'table'
+    def take_action_defaults(self, parsed_args):
+        super().take_action_defaults(parsed_args)
+        self.post_payload['limit'] = parsed_args.limit
+        self.post_payload['offset'] = parsed_args.offset
+        return self
 
     def take_action(self, parsed_args):
         # This needs to be more sophisticated - does not allow overrides etc
@@ -96,3 +119,13 @@ class TaccApisFormatMany(JsonVerbose, TasApiClient, BearerTokenFormatMany):
         # # for requests made directly via requests module
         # self.requests_client = Swaggerless(self.tapis_client)
         return ((), ())
+
+
+class TaccApisFormatManyUnlimited(TaccApisFormatMany):
+    def get_parser(self, prog_name):
+        parser = BearerTokenFormatMany.get_parser(self, prog_name)
+        parser = add_common_arguments(parser)
+        return parser
+
+    def take_action_defaults(self, parsed_args):
+        return self
