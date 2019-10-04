1e180886a2466c22f43736d908d8f96f03c1eda6
Matt Vaughn <vaughn@tacc.utexas.edu>
2019-10-04 07:36:09 -0500

    TAPIS_CLI_SHOW_CURL now defaults to the current value of TAPIS_PY_SHOW_CURL

--- tapis_cli/settings/display.py
+++ tapis_cli/settings/display.py
@@ -6,9 +6,10 @@ __all__ = [
     'SHOW_CURL'
 ]
 
-DATE_FORMAT = ns_os_environ_get("DATE_FORMAT", "YYYYMMDDTHHmmssZZ")
-PAGE_SIZE = int_or_none(ns_os_environ_get("PAGE_SIZE", "100"))
-LOG_LEVEL = ns_os_environ_get("LOG_LEVEL", None)
-RESPONSE_FORMAT = ns_os_environ_get("RESPONSE_FORMAT", None)
-FIT_WIDTH = parse_boolean(ns_os_environ_get("FIT_WIDTH", '1'))
-SHOW_CURL = parse_boolean(ns_os_environ_get("SHOW_CURL", '0'))
+DATE_FORMAT = ns_os_environ_get('DATE_FORMAT', 'YYYYMMDDTHHmmssZZ')
+PAGE_SIZE = int_or_none(ns_os_environ_get('PAGE_SIZE', '100'))
+LOG_LEVEL = ns_os_environ_get('LOG_LEVEL', None)
+RESPONSE_FORMAT = ns_os_environ_get('RESPONSE_FORMAT', None)
+FIT_WIDTH = parse_boolean(ns_os_environ_get('FIT_WIDTH', '1'))
+SHOW_CURL = parse_boolean(
+    ns_os_environ_get('SHOW_CURL', os.environ.get('TAPIS_PY_SHOW_CURL', '0')))
