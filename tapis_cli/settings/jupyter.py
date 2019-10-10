7688c60eae2b796cd99fdcce099d603378267717
Matt Vaughn <vaughn@tacc.utexas.edu>
2019-10-10 16:13:22 -0500

    Deactivated unused settings. Reworked seetings to honor AgavePy's default tenant ID setting.
    Closes #206

--- tapis_cli/settings/jupyter.py
+++ tapis_cli/settings/jupyter.py
@@ -1,9 +1,11 @@
 """TACC Jupyter-specific CLI settings"""
 import os
-from .organization import TENANT_DNS_DOMAIN
+# from .organization import TENANT_DNS_DOMAIN
 
-__all__ = ['TENANT_JUPYTER_SERVER', 'TENANT_JUPYTER_URI']
+__all__ = []
 
-TENANT_JUPYTER_SERVER = os.environ.get('TENANT_JUPYTER_SERVER',
-                                       'jupyter.' + TENANT_DNS_DOMAIN)
-TENANT_JUPYTER_URI = 'https://' + TENANT_JUPYTER_SERVER
+# __all__ = ['TENANT_JUPYTER_SERVER', 'TENANT_JUPYTER_URI']
+
+# TENANT_JUPYTER_SERVER = os.environ.get('TENANT_JUPYTER_SERVER',
+#                                        'jupyter.' + TENANT_DNS_DOMAIN)
+# TENANT_JUPYTER_URI = 'https://' + TENANT_JUPYTER_SERVER
--- tapis_cli/settings/organization.py
+++ tapis_cli/settings/organization.py
@@ -1,16 +1,13 @@
 import os
 from .helpers import (parse_boolean, ns_os_environ_get)
 
-__all__ = [
-    'TENANT_DNS_DOMAIN', 'TENANT_PROJECT_NAME', 'TENANT_API_SERVER',
-    'TENANT_PRIMARY_STORAGE_SYSTEM'
-]
+__all__ = []
 
-TENANT_DNS_DOMAIN = os.environ.get('TENANT_DNS_DOMAIN', 'tacc.utexas.edu')
+# TENANT_DNS_DOMAIN = os.environ.get('TENANT_DNS_DOMAIN', 'tacc.utexas.edu')
 
 # TACC.cloud TAS config
 # NOTE - These are mocks
-TENANT_PROJECT_NAME = os.environ.get('TENANT_PROJECT_NAME', 'TAPIS_SANDBOX')
+# TENANT_PROJECT_NAME = os.environ.get('TENANT_PROJECT_NAME', 'TAPIS_SANDBOX')
 # TACC_PROJECT_ID = os.environ.get('TACC_PROJECT_ID', '65536')
 # TACC_PROJECT_GROUP = os.environ.get('TACC_PROJECT_GROUP', '131072')
 
@@ -19,9 +16,9 @@ TENANT_PROJECT_NAME = os.environ.get('TENANT_PROJECT_NAME', 'TAPIS_SANDBOX')
 # NOTE - This is a mock
 # TACC_MANAGER_ACCOUNT = os.environ.get('TACC_MANAGER_ACCOUNT', 'tacolord')
 
-TENANT_API_SERVER = os.environ.get('TENANT_API_SERVER',
-                                   'https://api.' + TENANT_DNS_DOMAIN + '/')
+# TENANT_API_SERVER = os.environ.get('TENANT_API_SERVER',
+#                                    'https://api.' + TENANT_DNS_DOMAIN + '/')
 
-# NOTE - This is a mock
-TENANT_PRIMARY_STORAGE_SYSTEM = os.environ.get('TENANT_PRIMARY_STORAGE_SYSTEM',
-                                               'data-tapis-sandbox')
+# # NOTE - This is a mock
+# TENANT_PRIMARY_STORAGE_SYSTEM = os.environ.get('TENANT_PRIMARY_STORAGE_SYSTEM',
+#                                                'data-tapis-sandbox')
