825c5c1409d03e93dee51dc85fd9e8966bf93839
Matt Vaughn <vaughn@tacc.utexas.edu>
2019-10-16 19:23:40 -0400

    Added a Python2-compatible version of lru_cache. Still needs to be functionally tested.

--- tapis_cli/hashcache/lru_py27.py
+++ tapis_cli/hashcache/lru_py27.py
@@ -19,10 +19,16 @@ class _HashedSeq(list):
     def __hash__(self):
         return self.hashvalue
 
-def _make_key(args, kwds, typed,
-             kwd_mark = (object(),),
-             fasttypes = {int, str, frozenset, type(None)},
-             sorted=sorted, tuple=tuple, type=type, len=len):
+
+def _make_key(args,
+              kwds,
+              typed,
+              kwd_mark=(object(), ),
+              fasttypes={int, str, frozenset, type(None)},
+              sorted=sorted,
+              tuple=tuple,
+              type=type,
+              len=len):
     'Make a cache key from optionally typed positional and keyword arguments'
     key = args
     if kwds:
@@ -37,6 +43,7 @@ def _make_key(args, kwds, typed,
     elif len(key) == 1 and type(key[0]) in fasttypes:
         return key[0]
     return _HashedSeq(key)
+
 
 def lru_cache(maxsize=100, typed=False):
     """Least-recently-used cache decorator.
@@ -66,16 +73,16 @@ def lru_cache(maxsize=100, typed=False):
     def decorating_function(user_function):
 
         cache = dict()
-        stats = [0, 0]                  # make statistics updateable non-locally
-        HITS, MISSES = 0, 1             # names for the stats fields
+        stats = [0, 0]  # make statistics updateable non-locally
+        HITS, MISSES = 0, 1  # names for the stats fields
         make_key = _make_key
-        cache_get = cache.get           # bound method to lookup key or return None
-        _len = len                      # localize the global len() function
-        lock = RLock()                  # because linkedlist updates aren't threadsafe
-        root = []                       # root of the circular doubly linked list
-        root[:] = [root, root, None, None]      # initialize by pointing to self
-        nonlocal_root = [root]                  # make updateable non-locally
-        PREV, NEXT, KEY, RESULT = 0, 1, 2, 3    # names for the link fields
+        cache_get = cache.get  # bound method to lookup key or return None
+        _len = len  # localize the global len() function
+        lock = RLock()  # because linkedlist updates aren't threadsafe
+        root = []  # root of the circular doubly linked list
+        root[:] = [root, root, None, None]  # initialize by pointing to self
+        nonlocal_root = [root]  # make updateable non-locally
+        PREV, NEXT, KEY, RESULT = 0, 1, 2, 3  # names for the link fields
 
         if maxsize == 0:
 
@@ -90,7 +97,8 @@ def lru_cache(maxsize=100, typed=False):
             def wrapper(*args, **kwds):
                 # simple caching without ordering or size limit
                 key = make_key(args, kwds, typed)
-                result = cache_get(key, root)   # root used here as a unique not-found sentinel
+                result = cache_get(
+                    key, root)  # root used here as a unique not-found sentinel
                 if result is not root:
                     stats[HITS] += 1
                     return result
@@ -151,7 +159,8 @@ def lru_cache(maxsize=100, typed=False):
         def cache_info():
             """Report cache statistics"""
             with lock:
-                return _CacheInfo(stats[HITS], stats[MISSES], maxsize, len(cache))
+                return _CacheInfo(stats[HITS], stats[MISSES], maxsize,
+                                  len(cache))
 
         def cache_clear():
             """Clear the cache and cache statistics"""
