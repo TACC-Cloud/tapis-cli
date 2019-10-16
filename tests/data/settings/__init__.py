e5370aab4c73ebbf1cfbee091a9009316461682c
Matt Vaughn <vaughn@tacc.utexas.edu>
2019-10-16 14:47:58 -0500

    Removed deprecated settings names and use of the ENV_PREFIX

--- tests/data/settings/__init__.py
+++ tests/data/settings/__init__.py
@@ -4,5 +4,5 @@ __all__ = ['ENVS_AND_VALS']
 ENVS_AND_VALS = [("TAPIS_CLI_PAGE_SIZE", 9000),
                  ("TAPIS_CLI_LOG_LEVEL", "CRITICAL"),
                  ("TAPIS_CLI_RESPONSE_FORMAT", "yaml"),
-                 ("TENANT_DNS_DOMAIN", "tacc.dev"),
-                 ("TENANT_PROJECT_NAME", "TACO_SUPERPOWERS")]
+                 ("TAPIS_TENANTS_URL", "https://api.tacc.utexas.edu/tenants"),
+                 ("TAPIS_CLI_SHOW_CURL", False)]
--- tests/test_001_settings.py
+++ tests/test_001_settings.py
@@ -6,7 +6,7 @@ from .data import settings as settings_data
 def test_envs_are_settings(testing_envs):
     from tapis_cli import settings
     for k, v in settings_data.ENVS_AND_VALS:
-        k = k.replace(settings.ENV_PREFIX + '_', '')
+        # k = k.replace(settings.ENV_PREFIX + '_', '')
         vv = getattr(settings, k)
         assert v == vv, \
             'Env {} not propagated as a setting'.format(k)
