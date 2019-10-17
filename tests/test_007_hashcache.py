a0af579cc736b07fed731ac75f8220f20c91d6f9
Matt Vaughn <vaughn@tacc.utexas.edu>
2019-10-16 20:13:21 -0400

    Unit tests showing that both serializers can work with the backported lru_cache, at least in a trivial case

---
+++ tests/test_007_hashcache.py
@@ -0,0 +1,47 @@
+import pytest
+from time import sleep
+from tapis_cli.utils import seconds
+
+def test_jsoncache():
+    """Check that Python2/3 lru_cache functions both work w json serializer
+    """
+    from tapis_cli.hashcache import jsoncache, lru_cache
+    @jsoncache.mcache(lru_cache(maxsize=256))
+    def timer_function(duration=1):
+        sleep(duration)
+        return 'function_response'
+
+    delay = 2
+    start_01 = seconds()
+    resp = timer_function(delay)
+    start_02 = seconds()
+    resp = timer_function(delay)
+    end_01 = seconds()
+
+    delta_1 = round(start_02 - start_01)
+    delta_2 = round(end_01 - start_02)
+
+    assert delta_1 == delay
+    assert delta_2 < delay
+
+def test_picklecache():
+    """Check that Python2/3 lru_cache functions both work w cloudpickle serializer
+    """
+    from tapis_cli.hashcache import picklecache, lru_cache
+    @picklecache.mcache(lru_cache(maxsize=256))
+    def timer_function(duration=1):
+        sleep(duration)
+        return 'function_response'
+
+    delay = 2
+    start_01 = seconds()
+    resp = timer_function(delay)
+    start_02 = seconds()
+    resp = timer_function(delay)
+    end_01 = seconds()
+
+    delta_1 = round(start_02 - start_01)
+    delta_2 = round(end_01 - start_02)
+
+    assert delta_1 == delay
+    assert delta_2 < delay
