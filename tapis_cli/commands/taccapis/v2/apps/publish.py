cecad282825427c7fbd3d5670a9b4b62cd9039d0
Matt Vaughn <vaughn@tacc.utexas.edu>
2019-11-01 06:50:49 -0500

    Added support for specifying app name and version when publishing. Clarified the help text for specifying public app execution system.
    Updated TESTING text

--- tapis_cli/commands/taccapis/v2/apps/publish.py
+++ tapis_cli/commands/taccapis/v2/apps/publish.py
@@ -22,7 +22,19 @@ class AppsPublish(AppsFormatOne, ServiceIdentifier):
             '--execution-system',
             type=str,
             dest='public_execution_system',
-            help='Execution system where published app will run')
+            help='Optional execution system where the public app will run')
+        parser.add_argument(
+            '-n',
+            '--app-name',
+            type=str,
+            dest='app_name',
+            help='Optional new name of the public app')
+        parser.add_argument(
+            '-x',
+            '--app-version',
+            type=str,
+            dest='app_version',
+            help='Optional new version of the public app')
         return parser
 
     def take_action(self, parsed_args):
@@ -31,8 +43,14 @@ class AppsPublish(AppsFormatOne, ServiceIdentifier):
         headers = self.render_headers(App, parsed_args)
 
         mgt_body = {'action': 'publish'}
+
+        if parsed_args.app_name is not None:
+            mgt_body['name'] = parsed_args.app_name
+        if parsed_args.app_version is not None:
+            mgt_body['version'] = parsed_args.app_version
         if parsed_args.public_execution_system is not None:
             mgt_body['executionSystem'] = parsed_args.public_execution_system
+
         rec = self.tapis_client.apps.manage(appId=parsed_args.identifier,
                                             body=mgt_body)
 
